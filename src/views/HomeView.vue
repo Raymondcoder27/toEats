c299f6a953b607761a7ddd42a2c49d94ed2ed304
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-02 15:08:24 +0300

    adds tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import {ref} from 'vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+type restaurant ={
+  name: string
+  staus: string
+}
+const restaurants: restaurant[] = ref([
+  'KFC', 'Chicken Tonight', 'Cafe Javas'
+])
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+     <div class="h3 text-gray-600 text-underline font-semibold text-center pt-5">
+      <ul>
+        <li v-for="restaurant in restaurants" :key="restaurant.name">
+          {{ restaurant.name }}
+        </li>
+      </ul>
+     </div>
+     <pre>
+      {{restaurants}}
+     </pre>
+
   </main>
 </template>
